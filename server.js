d165fb03ae9293a78919ba2fedfbce3605c0e42c
demoemailid1101 <demo.emailid1101@gmail.com>
2017-05-07 18:15:52 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+// Start server
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function() {
+    console.log('listning to Port', app.get('port'));
+});
+
+var allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+};
+app.use(allowCrossDomain);
