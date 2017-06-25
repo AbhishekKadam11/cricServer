be2832e2e9e76134df645e2505aa6ad545514bf9
demoemailid1101 <demo.emailid1101@gmail.com>
2017-06-25 13:26:15 +0530

    -new async module added for parllel get request. -scorelist and ball by ball request is merge.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var request = require('request');
+var async = require("async");
 var app = express();
 
 //static value
@@ -61,25 +62,95 @@ apiRoutes.get('/matchlist', function (req, res) {
     });
 });
 
-apiRoutes.get('/scorelist/:u_id', function (req, res) {
-   // res.send(JSON.stringify(req));
- //   console.log(req);
-    request({
-        url: 'http://cricapi.com/api/cricketScore' + '?' + 'unique_id=' + req.params.u_id,
-        headers: {
-            'apikey': apikey
-        },
-        json: true
-    }, function (error, response, body) {
-        if (response.statusCode === 200) {
-            res.send(JSON.stringify(body, undefined, 2));
-        }
-        else {
-            res.send("Unable to fatch record", response);
-        }
+// apiRoutes.get('/scorelist/:u_id', function (req, res) {
+//    // res.send(JSON.stringify(req));
+//  //   console.log(req);
+//     request({
+//         url: 'http://cricapi.com/api/cricketScore' + '?' + 'unique_id=' + req.params.u_id,
+//         headers: {
+//             'apikey': apikey
+//         },
+//         json: true
+//     }, function (error, response, body) {
+//         if (response.statusCode === 200) {
+//             ballSummary(req.params.u_id, function(result){
+//                 var balldetails = result;
+//             });
+//
+//             var matchscore = body;
+//             res.send(JSON.stringify(body, undefined, 2));
+//         }
+//         else {
+//             res.send("Unable to fatch record", response);
+//         }
+//
+//     });
+// });
 
+    function ballSummary(uid, result) {
+        request({
+            url: 'http://cricapi.com/api/ballByBall',
+            headers: {
+                'apikey': apikey,
+                'unique_id': uid
+            },
+            json: true
+        }, function (error, response, body) {
+            if (response.statusCode === 200) {
+                result = body;
+              //  res.send(JSON.stringify(body, undefined, 2));
+            }
+            else {
+                res.send("Unable to fatch record", response);
+            }
+
+        });
+    }
+
+apiRoutes.get('/scorelist/:u_id', function (req, res) {
+    async.parallel([
+        function (next) {
+            request({
+                url: 'http://cricapi.com/api/cricketScore' + '?' + 'unique_id=' + req.params.u_id,
+                headers: {
+                    'apikey': apikey
+                },
+                json: true
+            }, function (error, response, body) {
+                if (response.statusCode === 200) {
+                    next(null, body);
+                }
+                else {
+                    res.send("Unable to fatch record", response);
+                }
+            });
+        },
+        function (next) {
+            request({
+                url: 'http://cricapi.com/api/ballByBall' + '?' + 'unique_id=' + req.params.u_id,
+                headers: {
+                    'apikey': apikey
+                },
+                json: true
+            }, function (error, response, body) {
+                if (response.statusCode === 200) {
+                    //   balldetails = body;
+                    next(null, body);
+                    //    result = body;
+                    //  res.send(JSON.stringify(body, undefined, 2));
+                }
+                else {
+                    res.send("Unable to fatch record", response);
+                }
+            });
+
+        }], function (err, results) {
+        // results is [firstData, secondData]
+        //   console.log(matchscore,balldetails);
+        res.send(results);
     });
 });
+
 
 apiRoutes.get('/playerstateist', function (req, res) {
     request({
@@ -100,24 +171,24 @@ apiRoutes.get('/playerstateist', function (req, res) {
     });
 });
 
-apiRoutes.get('/ballbyball', function (req, res) {
-    request({
-        url: 'http://cricapi.com/api/ballByBall',
-        headers: {
-            'apikey': apikey,
-            'unique_id': req.uid
-        },
-        json: true
-    }, function (error, response, body) {
-        if (response.statusCode === 200) {
-            res.send(JSON.stringify(body, undefined, 2));
-        }
-        else {
-            res.send("Unable to fatch record", response);
-        }
-
-    });
-});
+// apiRoutes.get('/ballbyball', function (req, res) {
+//     request({
+//         url: 'http://cricapi.com/api/ballByBall',
+//         headers: {
+//             'apikey': apikey,
+//             'unique_id': req.uid
+//         },
+//         json: true
+//     }, function (error, response, body) {
+//         if (response.statusCode === 200) {
+//             res.send(JSON.stringify(body, undefined, 2));
+//         }
+//         else {
+//             res.send("Unable to fatch record", response);
+//         }
+//
+//     });
+// });
 
 apiRoutes.get('/summary', function (req, res) {
     request({
@@ -137,3 +208,22 @@ apiRoutes.get('/summary', function (req, res) {
 
     });
 });
+
+// apiRoutes.get('/livescore', function (req, res) {
+//     request({
+//         url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20cricket.scorecard.live&format=json&diagnostics=true&env=store%3A%2F%2F0TxIGQMQbObzvU4Apia0V0&callback=',
+//         // headers: {
+//         //     'apikey': apikey,
+//         //     'pid': req.pid
+//         // },
+//         json: true
+//     }, function (error, response, body) {
+//         if (response.statusCode === 200) {
+//             res.send(JSON.stringify(body, undefined, 2));
+//         }
+//         else {
+//             res.send("Unable to fatch record", response);
+//         }
+//
+//     });
+// });
